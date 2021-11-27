6358c7da79d81aaaec75245f9ba883218d84ca11
Martin Kühl <martin.kuehl@posteo.net>
2021-11-27 21:17:28 +0100

    npm run fix

--- src/test/suite/status.test.ts
+++ src/test/suite/status.test.ts
@@ -5,7 +5,7 @@ import * as status from '../../status';
 describe('the status item', () => {
 	const item = status.item;
 
-	it('loads during the callback', async()=>{
+	it('loads during the callback', async () => {
 		const promise = sinon.promise();
 		const watch = status.watch(() => promise);
 		assert.strict.equal(item.command, undefined);
@@ -27,7 +27,7 @@ describe('the status item', () => {
 		const watch = status.watch(() => promise);
 		try {
 			await watch;
-		} catch(e) {
+		} catch (e) {
 			assert.strict.equal(e, 2);
 			assert.strict.equal(item.command, 'direnv.reload');
 			assert.strict.equal(item.text, `${status.icon}${status.no}`);
