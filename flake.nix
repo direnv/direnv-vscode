c36a7d6f79d6463b1da53b6484705f24ad4b8361
Martin Kühl <martin.kuehl@posteo.net>
2021-12-05 23:57:10 +0100

    nix: use simpleFlake

--- flake.nix
+++ flake.nix
@@ -10,34 +10,35 @@
   outputs = { self, flake-utils, nixpkgs, ... }:
     let
       attrs = nixpkgs.lib.importJSON ./package.json;
-      name = attrs.name;
+      inherit (attrs) name version;
+      vsix = "${name}-${version}.vsix";
     in
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        vsix = "${attrs.name}-${attrs.version}.vsix";
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        defaultPackage = self.packages.${system}.vsix;
+    flake-utils.lib.simpleFlake {
+      inherit name self nixpkgs;
+      overlay = this: prev: {
+        ${name} = {
+          vsix = prev.mkYarnPackage {
+            src = ./.;
+            name = vsix;
+            buildPhase = ''
+              # fix Error: ENOENT: no such file or directory, stat '/build/$HASH-source/deps/${name}/${name}
+              rm deps/${name}/${name}
+              # fix Warning: Using '*' activation is usually a bad idea as it impacts performance.
+              echo y |
+              yarn run --offline vsce package --yarn
+            '';
+            installPhase = ''
+              mv deps/${name}/${vsix} $out
+            '';
+            distPhase = "true";
+          };
 
-        packages.vsix = pkgs.mkYarnPackage {
-          src = ./.;
-          name = vsix;
-          buildPhase = ''
-            # fix Error: ENOENT: no such file or directory, stat '/build/$HASH-source/deps/${name}/${name}
-            rm deps/${name}/${name}
-            # fix Warning: Using '*' activation is usually a bad idea as it impacts performance.
-            echo y |
-            yarn run --offline vsce package --yarn
-          '';
-          installPhase = ''
-            mv deps/${name}/${vsix} $out
-          '';
-          distPhase = "true";
-        };
+          defaultPackage = this.${name}.vsix;
 
-        devShell = pkgs.mkShell {
-          inputsFrom = [ self.defaultPackage.${system} ];
+          devShell = prev.mkShell {
+            inputsFrom = [ this.${name}.defaultPackage ];
+          };
         };
-      });
+      };
+    };
 }
