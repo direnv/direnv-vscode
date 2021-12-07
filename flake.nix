9cafff68dcde1de3cab77ed6efd6a10807db4e28
Martin Kühl <martin.kuehl@posteo.net>
2021-12-07 20:35:25 +0100

    flake: simplify

--- flake.nix
+++ flake.nix
@@ -10,11 +10,11 @@
   outputs = { self, flake-utils, nixpkgs, ... }:
     let
       attrs = nixpkgs.lib.importJSON ./package.json;
-      name = attrs.name;
+      inherit (attrs) name version;
+      vsix = "${name}-${version}.vsix";
     in
     flake-utils.lib.eachDefaultSystem (system:
       let
-        vsix = "${attrs.name}-${attrs.version}.vsix";
         pkgs = import nixpkgs { inherit system; };
       in
       {
