d7cac374c170a3422f94d3e1c0df433b7311c58a
Martin Kühl <martin.kuehl@posteo.net>
2022-01-30 17:12:06 +0100

    nix: add checks

--- flake.nix
+++ flake.nix
@@ -16,26 +16,58 @@
       vsix = "${name}-${version}.vsix";
     in
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        npm = pkgs.callPackage npmlock2nix { };
-        node_modules_attrs = {
-          buildInputs = [ pkgs.python3 pkgs.pkg-config pkgs.libsecret ];
-        };
-      in
-      {
-        defaultPackage = self.packages.${system}.vsix;
+      if system == "i686-linux" then { } # unsupported by vscode
+      else
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = { allowUnfree = true; };
+          };
+          npm = pkgs.callPackage npmlock2nix { };
+          node_modules_attrs = {
+            buildInputs =
+              if pkgs.stdenv.isDarwin
+              then [ pkgs.python3 ]
+              else [ pkgs.python3 pkgs.pkg-config pkgs.libsecret ];
+          };
+        in
+        {
+          defaultPackage = self.packages.${system}.vsix;
 
-        packages.vsix = npm.build {
-          src = ./.;
-          inherit node_modules_attrs;
-          buildCommands = [ "npm run package" ];
-          installPhase = "mv ${vsix} $out";
-        };
+          packages.vsix = npm.build {
+            src = ./.;
+            inherit node_modules_attrs;
+            buildCommands = [ "npm run package" ];
+            installPhase = "mv ${vsix} $out";
+          };
 
-        devShell = npm.shell {
-          src = ./.;
-          inherit node_modules_attrs;
-        };
-      });
+          devShell = npm.shell {
+            src = ./.;
+            inherit node_modules_attrs;
+            packages = if pkgs.stdenv.isDarwin then [ ] else [ pkgs.xvfb-run ];
+          };
+
+          checks.test =
+            let
+              vscode-bin =
+                if pkgs.stdenv.isDarwin
+                then "${pkgs.vscode}/Applications/Visual Studio Code.app/Contents/MacOS/Electron"
+                else "${pkgs.vscode}/lib/vscode/code";
+              xvfb-run =
+                if pkgs.stdenv.isDarwin
+                then ""
+                else "${pkgs.xvfb-run}/bin/xvfb-run --auto-servernum";
+            in
+            npm.build {
+              src = ./.;
+              inherit node_modules_attrs;
+              buildInputs = [ pkgs.direnv ];
+              installPhase = "touch $out";
+              buildCommands = [
+                "mkdir .home"
+                "export HOME=$PWD/.home"
+                "${xvfb-run} npm test -- '${vscode-bin}'"
+              ];
+            };
+        });
 }
--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -2,18 +2,19 @@ import * as path from 'path'
 
 import { runTests } from '@vscode/test-electron'
 
-async function main() {
+async function main(vscodeExecutablePath?: string) {
 	try {
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../')
 		const extensionTestsPath = path.resolve(__dirname, './suite/index')
 		const workspacePath = path.resolve(__dirname, '../../test/workspace')
 		const disableExtensions = '--disable-extensions'
 		const launchArgs = [workspacePath, disableExtensions]
-		await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs })
+		await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath, launchArgs })
 	} catch (err) {
 		console.error('Failed to run tests')
 		process.exit(1)
 	}
 }
 
-void main()
+const executable = process.argv[2]
+void main(executable)
