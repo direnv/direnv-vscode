b938abeb988d5d834f1fa341a1d6200b45819781
Martin Kühl <martin.kuehl@posteo.net>
2022-12-02 19:44:52 +0100

    nix: refer to ./. with fixed name

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
   }: let
     inherit (nixpkgs.lib.importJSON ./package.json) name version;
     vsix = "${name}-${version}.vsix";
+    this = builtins.path {
+      path = ./.;
+      inherit name;
+    };
   in
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
@@ -22,7 +26,7 @@
       packages.default = self.packages.${system}.vsix;
 
       packages.vsix = pkgs.mkYarnPackage {
-        src = ./.;
+        src = this;
         configurePhase = "ln -s $node_modules node_modules";
         buildPhase = "yarn run package";
         installPhase = "mv ${vsix} $out";
